0cc8947aa23df5cf875be8e3f7dd01c646a9ac51
FranciscoFunes <ffunes90@gmail.com>
2023-10-10 22:50:44 -0300

    fix notify endpoint

--- src/app/api/notify/route.ts
+++ src/app/api/notify/route.ts
@@ -1,16 +1,17 @@
-import { NextApiRequest, NextApiResponse } from 'next';
 import mercadopago from 'mercadopago';
+import { NextApiRequest, NextApiResponse } from 'next';
 
 mercadopago.configure({
 	access_token: process.env.NEXT_ACCESS_TOKEN!,
 });
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+export async function GET(req: NextApiRequest, res: NextApiResponse) {
 	const { query } = req;
 
 	const topic = query.topic || query.type;
 
 	console.log({ query, topic });
+
 	try {
 		if (topic === 'payment') {
 			const paymentId = query.id || query['data.id'];
@@ -18,10 +19,31 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 			let paymentStatus = payment.body.status;
 
 			console.log([payment, paymentStatus]);
-		}
-	} catch (error) {
-		res.send(error);
-	}
-};
 
-export default handler;
+			// Return a success response with payment status
+			return new Response(JSON.stringify({ paymentStatus }), {
+				status: 200,
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			});
+		} else {
+			// Handle other cases if needed and return appropriate responses
+			return new Response(JSON.stringify({ message: 'Topic not supported' }), {
+				status: 400,
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			});
+		}
+	} catch (error: any) {
+		// Specify the error type as 'any'
+		// Handle errors and return an error response
+		return new Response(JSON.stringify({ error: error.message }), {
+			status: 500,
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+	}
+}
